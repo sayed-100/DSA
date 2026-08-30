ba8253c0ec2b8aa590a0c470e1bb91cf4631a933
sayed-100 <sayedsaifali2006@gmail.com>
2026-08-31 02:36:47 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (43.22%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         int low = 0, high = nums.size() - 1;
-        int ans = nums.size();
+        // int ans = nums.size();
         while ( low <= high )
         {
             int mid = low  + ( ( high - low ) / 2 );
             if( nums[mid] >= target )
             {
-                ans = mid;
+                // ans = mid;
                 high = mid - 1;
             }
             else
@@ -16,6 +16,6 @@ public:
                 low = mid + 1;
             }
         }
-        return ans;
+        return low;
     }
 };
