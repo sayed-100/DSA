6103ee8a6f65cd1ee2e7063ccb89f9edd6c6b16f
sayed-100 <sayedsaifali2006@gmail.com>
2026-09-02 16:05:10 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (28.43%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -1,44 +1,24 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int low  = 0, high = nums.size() - 1;
-        int ans = INT_MAX;
-        while (low <= high)
+        int low = 0, high = nums.size() - 1;
+        while( low < high)
         {
-            // 1. If strictly sorted, the first element is the minimum of this range
-            if (nums[low] < nums[high]) {
-                ans = min(ans, nums[low]);
-                break;
-            }
-            int mid  = low + ( high - low ) / 2;
-           
-            //duplicate present in array at low, mid and high then shrink
-            if(nums[low] == nums[mid] && nums[mid] == nums[high])
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > nums[high])
             {
-                ans = min(ans, nums[low]);
-                low++;
-                high--;
-                continue;
-            }
-            // 3. Two-way duplicates at left boundary: safely skip one element
-            if (nums[low] == nums[mid]) {
-                ans = min(ans, nums[low]);
-                low++;
-                continue;
-            }
-            //left sorted
-            if(nums[low] <= nums[mid])
-            {
-                ans = min(ans, nums[low]);
                 low = mid + 1;
             }
-            // right sorted
+            else if(nums[mid] < nums[high])
+            {
+                high = mid;
+            }
             else
             {
-                ans = min( ans, nums[mid]);
-                high = mid - 1;
+                high --;
             }
+
         }
-        return ans;
+        return nums[low];
     }
 };
